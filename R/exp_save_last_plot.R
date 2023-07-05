dbfc1afad5ef2f2f56f545d3e8fd798a6a2c52ef
Desi Joshua Quintans <science@desiquintans.com>
2023-07-05 14:51:30 +1000

    Add save_last_plot()

---
+++ R/exp_save_last_plot.R
@@ -0,0 +1,35 @@
+
+#' Save the most recent plot to a file
+#'
+#' @param filename (Character) The path and filename of the file to create.
+#' @param size (Character) The output size of the file, in the form `"width x height unit`. For example:
+#'      - `"5 x 7 in"`
+#'      - `"12 x 8 cm"`
+#'      - `"300 x 150 mm"`
+#'      - `"1920 x 1080 px"`
+#' @param dpi (Integer) The resolution (dots per inch) of the output file.
+#' @param ... Other arguments passed to [ggplot2::ggsave()].
+#'
+#' @return This function returns nothing, but has the side-effect of writing a file to `filename`.
+#' @export
+#'
+#' @examples
+#' \dontrun {
+#' plot_novelty(interview_scores)
+#'
+#' save_last_plot("my_file.png", "8 x 6 in")
+#' }
+#'
+#' @md
+save_last_plot <- function(filename, size, dpi = 300, ...) {
+    split_size <- unlist(regmatches(size,
+                                    regexec("([0-9.]+)\\s*(x|\\s)\\s*([0-9.]+)\\s*(.+)",
+                                            size)))
+
+    ggplot2::ggsave(filename = filename, plot = ggplot2::last_plot(),
+                    width = split_size[2], height = split_size[4],
+                    units = split_size[5], dpi = dpi, ...)
+}
+
+
+
